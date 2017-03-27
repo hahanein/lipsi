447cede7126e3c393937315e0df474687f02488f
hahanein <binjamin@riseup.net>
2017-03-27 19:56:43 +0200

    normal form algorithm basic idea

--- src/lib.rs
+++ src/lib.rs
@@ -25,9 +25,9 @@ trait SetOperations {
     fn complement(&self) -> Self;
     fn reverse(&self) -> Self;
     fn sort(&self) -> Self;
-    // fn shift(&self, i8) -> Self;
+    fn shift(&self, usize) -> Self;
     fn zero(&self) -> Self;
-    // fn normal(&self) -> Self;
+    fn normal(&self) -> Self;
     // fn reduced(&self) -> Self;
     // fn prime(&self) -> Self;
 }
@@ -53,11 +53,36 @@ impl SetOperations for PcSet {
         }
         clone
     }
+    fn shift(&self, n: usize) -> PcSet {
+        let len = self.len();
+        self.iter()
+            .cycle()
+            .skip(n + 1)
+            .take(len)
+            .cloned()
+            .collect()
+    }
     fn zero(&self) -> PcSet {
         match self.iter().min() {
             Some(min) => self.iter().map(|x| x - min).collect(),
             None => self.clone(),
         }
+    }
+    fn normal(&self) -> PcSet {
+        let sorted = self.sort();
+        let len = self.len();
+        (0..len)
+            .map(|x| sorted.shift(x))
+            .fold(None, |acc, x| {
+                match acc {
+                    None => Some(x),
+                    Some(acc) => {
+                        if x[len - 1] - x[0] < acc[len - 1] - acc[0] { Some(x) }
+                        else { Some(acc) }
+                    },
+                }
+            })
+            .unwrap()
     }
 }
 
@@ -98,8 +123,21 @@ mod tests {
     }
 
     #[test]
+    fn shift() {
+        let x: PcSet = vec![1, 2, 3];
+        assert_eq!(x.shift(1), vec![3, 1, 2]);
+        assert_eq!(x, vec![1, 2, 3]);
+    }
+
+    #[test]
     fn zero() {
         let x: PcSet = vec![1, 2, 3];
         assert_eq!(x.zero(), vec![0, 1, 2]);
+    }
+
+    #[test]
+    fn normal() {
+        let x: PcSet = vec![1, 2, 3];
+        assert_eq!(x.normal(), vec![0, 1, 2]);
     }
 }
